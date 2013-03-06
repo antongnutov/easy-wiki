104f98f2790557e98afe2c5ecb5b931801e79ee9
anton.gnutov <gnutov@gmail.com>
2013-03-06 16:49:32 +0400

    bootstrap improved a bit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,7 +13,9 @@ class BootStrap {
         
         switch(Environment.current){
             case Environment.DEVELOPMENT:
-                new WikiPage(name: 'Home', syntax: 'Confluence', content: 'h1. Welcome to Wiki!').save()    
+                def home = new WikiPage(name: 'Home', syntax: 'Confluence', content: 'h1. Welcome to Wiki!').save()
+                new WikiPage(name: 'Page1', syntax: 'Confluence', content: 'h2. Test page', parentPage: home).save()
+                new WikiPage(name: 'Page2', syntax: 'Confluence', content: 'h2. Test page', parentPage: home).save()
             break;
         }
     }
@@ -40,34 +42,34 @@ class BootStrap {
             UserRole.create(user, wikiRole)
         }
     }
-    
+
     def printDBSettings() {
         String VCAP_SERVICES = System.getenv('VCAP_SERVICES')
         println "VCAP_SERVICES: ${VCAP_SERVICES}"
-  
-        try {
-           def servicesMap = JSON.parse(VCAP_SERVICES)
-           servicesMap.each { key, services ->
-              if (key.startsWith('mysql')) {
-                 for (service in services) {
-                    print "MySQL service $service.name: "
-                    print "url='jdbc:mysql://$service.credentials.hostname:$service.credentials.port/$service.credentials.name', "
-                    print "user='$service.credentials.user', "
-                    println "password='$service.credentials.password'n"
-                 }
-              }
-              else if (key.startsWith('postgresql')) {
-                 for (service in services) {
-                    print "PostgreSQL service $service.name: "
-                    print "url='jdbc:postgresql://$service.credentials.hostname:$service.credentials.port/$service.credentials.name', "
-                    print "user='$service.credentials.user', "
-                    println "password='$service.credentials.password'n"
-                 }
-              }
-           }
-        }
-        catch (e) {
-           println "Error occurred parsing VCAP_SERVICES: $e.message"
+
+        if (VCAP_SERVICES) {
+            try {
+                def servicesMap = JSON.parse(VCAP_SERVICES)
+                servicesMap.each { key, services ->
+                    if (key.startsWith('mysql')) {
+                        for (service in services) {
+                            print "MySQL service $service.name: "
+                            print "url='jdbc:mysql://$service.credentials.hostname:$service.credentials.port/$service.credentials.name', "
+                            print "user='$service.credentials.user', "
+                            println "password='$service.credentials.password'n"
+                        }
+                    } else if (key.startsWith('postgresql')) {
+                        for (service in services) {
+                            print "PostgreSQL service $service.name: "
+                            print "url='jdbc:postgresql://$service.credentials.hostname:$service.credentials.port/$service.credentials.name', "
+                            print "user='$service.credentials.user', "
+                            println "password='$service.credentials.password'n"
+                        }
+                    }
+                }
+            } catch (e) {
+                println "Error occurred parsing VCAP_SERVICES: $e.message"
+            }
         }
     }
 }
